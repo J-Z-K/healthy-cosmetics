466a1fdb7d8aff1b872c3d49054c90b6e9974b10
Michal Kadzik <michalkadzik2000@gmail.com>
2020-08-25 11:16:44 +0200

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const [outputText, setOutputText] = useState(
+        "Jak smarujesz czymś swoją mordkę to chcesz żeby było to zdrowe!!!",
+    );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{outputText}</Text>
+            <Button
+                title="Sprawdź czy zdrowe"
+                onPress={() => setOutputText("Jescze nie wiem :(")}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    text: {
+        fontSize: 20,
+        textAlign: "center",
+    },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# healthy-cosmetics
+## zdrowe kosmetki
+this is POC of app that provides info about quality of ingridients in beauty products
